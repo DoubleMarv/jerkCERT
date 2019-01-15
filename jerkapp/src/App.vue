45f726821ca3e8a07dfb38b378a1c633d80bf3a2
Barrytron <barry@doodlecreative.ie>
2019-01-15 16:24:05 +0000

    Vue Setup App
    Let's go

---
+++ jerkapp/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <Createthecert/>
+  </div>
+</template>
+
+<script>
+import Createthecert from './components/create-the-cert.vue'
+
+export default {
+  name: 'app',
+  components: {
+    Createthecert
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
